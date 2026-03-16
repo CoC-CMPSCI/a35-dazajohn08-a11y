748626c9c2c7f035c2cf848cc37767cc6106f99a
John <dazajohn08@gmail.com>
2026-03-15 19:42:48 -0700

    done 3

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int main()
     // TODO: compute the total summation
 total = rdnum1 + rdnum2 + rdnum3;
     // TODO: print the 3 random numbers on the first line
-cout << "Line 1 has " << rdnum1 << " " << rdnum2 << " " << rdnum3 << endl;
+cout << "Line 1 has " << rdnum1 << " " << rdnum2 << " " << rdnum3 << " integers" <<endl;
 cout << "Line 2 has " << total << " " << total / 3 << endl;
     // TODO: print the total and average (integer division) on the second line
 
